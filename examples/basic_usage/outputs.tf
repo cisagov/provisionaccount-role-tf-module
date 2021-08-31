707a576ff36a3464fb068d76edd7d852d057daa5
David Redmin <david.redmin@trio.dhs.gov>
2021-08-31 17:26:35 -0400

    Use correct output provisionaccount-role-tf-module in example

--- examples/basic_usage/outputs.tf
+++ examples/basic_usage/outputs.tf
@@ -1,4 +1,4 @@
 output "provisionaccount_role_arn" {
-  value       = module.provisionaccount.provisionaccount_role_arn
+  value       = module.provisionaccount.provisionaccount_role.arn
   description = "The ARN of the IAM role that allows sufficient permissions to provision all AWS resources in the example Pettifogger0 account."
 }
